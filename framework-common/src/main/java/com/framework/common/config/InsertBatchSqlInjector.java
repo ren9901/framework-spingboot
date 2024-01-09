af9f47ba7b205fc1f6a7edb7fe4d15db710161ff
ren <ren@qq.com>
2024-01-09 17:00:04 +0800

    更改mp版本

--- framework-common/src/main/java/com/framework/common/config/InsertBatchSqlInjector.java
+++ framework-common/src/main/java/com/framework/common/config/InsertBatchSqlInjector.java
@@ -13,12 +13,12 @@ import java.util.List;
  * @description: SQL 注入器
  */
 public class InsertBatchSqlInjector extends DefaultSqlInjector {
-//    @Override
-//    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
-//        // super.getMethodList() 保留 Mybatis Plus 自带的方法
-//        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
-//        // 添加自定义方法：批量插入，方法名为 insertBatchSomeColumn
-//        methodList.add(new InsertBatchSomeColumn());
-//        return methodList;
-//    }
+    @Override
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        // super.getMethodList() 保留 Mybatis Plus 自带的方法
+        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
+        // 添加自定义方法：批量插入，方法名为 insertBatchSomeColumn
+        methodList.add(new InsertBatchSomeColumn());
+        return methodList;
+    }
 }
